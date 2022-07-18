c56d2516dae7a4638358344cef708d40fe65f280
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-07-18 17:15:47 -0400

    mysql now connected to server properly

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Greyson100!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 app.use((req, res) => {
     res.status(404).end();
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
